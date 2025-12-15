00b20367d9fb3c587b20e543aa183dcc107f15c0
Shashank <spandey21217@gmail.com>
2025-12-15 21:56:34 +0530

    database setup and signup

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import path from "path";
 
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
+import { connectDB } from "./lib/db.js";
 
 dotenv.config({ quiet: true });
 
@@ -12,10 +13,12 @@ const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json()) // req.body
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-// make ready for deployement
+// make ready for deployment
 if(process.env.NODE_ENV == "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -26,4 +29,7 @@ if(process.env.NODE_ENV == "production"){
     });
 }
 
-app.listen(PORT, () => console.log("Server running on port: " + PORT));
+app.listen(PORT, () => {
+    console.log("Server running on port: " + PORT);
+    connectDB();
+});
