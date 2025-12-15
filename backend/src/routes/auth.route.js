92e1b501dd2dbdba00fcdd144c1dce27fc3e46e3
Shashank <spandey21217@gmail.com>
2025-12-15 20:17:24 +0530

    deployment ready

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,15 +2,15 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/signup", (req,res) => {
+router.get("/signup", (req, res) => {
     res.send("Signup endpoint");
 });
 
-router.get("/login", (req,res) => {
+router.get("/login", (req, res) => {
     res.send("Login endpoint");
 });
 
-router.get("/logout", (req,res) => {
+router.get("/logout", (req, res) => {
     res.send("Logout endpoint");
 });
 
